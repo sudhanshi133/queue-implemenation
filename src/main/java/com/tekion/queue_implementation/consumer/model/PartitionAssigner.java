5831dbea30124cd9159ce11d305a6344c4854cc6
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-01-19 17:17:14 +0530

    producer added

--- src/main/java/com/tekion/queue_implementation/consumer/model/PartitionAssigner.java
+++ src/main/java/com/tekion/queue_implementation/consumer/model/PartitionAssigner.java
@@ -2,6 +2,7 @@ package com.tekion.queue_implementation.consumer.model;
 
 import com.tekion.queue_implementation.producer.mappings.Partition;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.*;
 
@@ -11,8 +12,8 @@ public class PartitionAssigner {
     private final List<Consumer> consumers;
     private int nextConsumerIndex = 0;
 
-    public PartitionAssigner(List<Consumer> consumers) {
-        this.consumers = consumers;
+    public PartitionAssigner(ConsumerRegistry registry) {
+        this.consumers = registry.getConsumers();
     }
 
     public Consumer assignConsumer(Partition partition) {
