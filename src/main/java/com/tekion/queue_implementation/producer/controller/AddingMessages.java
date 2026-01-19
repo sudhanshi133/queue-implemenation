7059e9579ebb7456f6de312bf5333bb1be948363
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-01-19 16:30:13 +0530

    producer added

--- src/main/java/com/tekion/queue_implementation/producer/controller/AddingMessages.java
+++ src/main/java/com/tekion/queue_implementation/producer/controller/AddingMessages.java
@@ -6,16 +6,13 @@ import com.tekion.queue_implementation.producer.model.Message;
 import com.tekion.queue_implementation.producer.service.AddToQueue;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller("/message")
+@RestController
+@RequestMapping("/message")
 public class AddingMessages {
 
     @Autowired
